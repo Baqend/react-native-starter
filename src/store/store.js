26a1d11eb0dabe76b1a9b642de2476adf61f9a91
Julian Tiemann <jutiemann@gmail.com>
2017-05-30 13:23:30 +0200

    add react native devicestorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,38 @@
+import { AsyncStorage } from 'react-native'
+
 import { applyMiddleware, combineReducers } from 'redux'
 
 import { createStoreWithBaqend, baqendReducer } from 'redux-baqend'
 import middlewares from '../middleware'
 import reducers from '../reducers'
 
-import { db } from 'baqend/lib/baqend'
+import { db, util } from 'baqend/lib/baqend'
+
+export class DeviceStorage extends util.TokenStorage {
+
+  constructor(origin, token) {
+    super(origin, token);
+  }
+
+  _saveToken(origin, token, temporary) {
+    if (token) {
+      AsyncStorage.setItem('@Baqend:' + origin, token);
+    } else {
+      AsyncStorage.removeItem('@Baqend:' + origin);
+    }
+  };
+
+}
+
+db.configure({
+  tokenStorageFactory: {
+    create: origin => {
+      return AsyncStorage.getItem('@Baqend:' + origin).then((token) => {
+        return new DeviceStorage(origin, token);
+      })
+    }
+  }
+})
 
 export default (initialState = {}) => {
   const reducer = combineReducers({
